30ca0777825db6201d8913a28f1930a20f3009ea
angelinag <andji.gash@gmail.com>
2020-10-23 21:01:03 +0300

    Initial commit

---
+++ Assets/Scripts/CameraScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScroll : MonoBehaviour
+{
+
+    private float scrollSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += Vector3.right * scrollSpeed;
+        transform.position = new Vector3(transform.position.x * scrollSpeed, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Platform/Generator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Platformer.Core;
+using Platformer.Model;
+using UnityEngine;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// This class generates new sections.
+    /// </summary> 
+    class Generator : MonoBehaviour
+    {
+        public PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        //define an Array
+        public static GameObject[] sections;
+        //I used this to keep track of the number of objects I spawned in the scene.
+        public static int numSpawned = 0;
+        public int numOfSections = 0;
+
+        void Start()
+        {
+            //Important note: place your prefabs folder(or levels or whatever) 
+            //in a folder called "Resources" like this "Assets/Resources/Sections"
+            sections = Resources.LoadAll<GameObject>("Sections");
+            numOfSections = sections.Length;
+        }
+
+        void SpawnRandomSection()
+        {
+            //spawns item in array position between 0 and 100
+            int whichItemIndex = Random.Range(0, numOfSections-1);
+
+            GameObject myObj = Instantiate(sections[whichItemIndex]) as GameObject;
+
+            numSpawned++;
+
+            myObj.transform.position = transform.position;
+        }
+        void Update()
+        {
+            if(numSpawned<1)
+            {
+                //where your instantiated object spawns from
+                transform.position = new Vector2(1,2);
+                SpawnRandomSection();
+            }
+        }
+
+    }
+}
+
